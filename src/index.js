b10c9bbed9c63dfd23df0a3e2e0d4f230a763454
vitalya0729 <36700292+vitalya0729@users.noreply.github.com>
2018-02-22 11:19:05 +0200

    not all

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.length = 0;
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
+    this.length++;
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return.this.length;
   }
 
   toArray() {
     // your implementation
+    return.this.arr;    
   }
 
   sort(indices) {
